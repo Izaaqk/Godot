0517b0cc68f7e8536d3f9b63e983d2e1139c1a22
A Thousand Ships <96648715+AThousandShips@users.noreply.github.com>
2024-05-14 14:28:43 +0200

    Add uses

--- editor/gui/scene_tree_editor.cpp
+++ editor/gui/scene_tree_editor.cpp
@@ -1686,7 +1686,7 @@ void SceneTreeDialog::_notification(int p_what) {
 		} break;
 
 		case NOTIFICATION_ENTER_TREE: {
-			connect("confirmed", callable_mp(this, &SceneTreeDialog::_select));
+			connect(SceneStringName(confirmed), callable_mp(this, &SceneTreeDialog::_select));
 		} break;
 
 		case NOTIFICATION_THEME_CHANGED: {
@@ -1698,7 +1698,7 @@ void SceneTreeDialog::_notification(int p_what) {
 		} break;
 
 		case NOTIFICATION_EXIT_TREE: {
-			disconnect("confirmed", callable_mp(this, &SceneTreeDialog::_select));
+			disconnect(SceneStringName(confirmed), callable_mp(this, &SceneTreeDialog::_select));
 		} break;
 	}
 }
